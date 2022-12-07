b461668037496e933a7e6ef78290dc9044debd8e
Maruo.S <raspi-maru2004@outlook.jp>
2022-12-07 10:39:11 +0900

    add some middleware

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,16 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", scopes={"public":"Get public data."})
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["api.marusoftware.net", "localhost"])
+
+from fastapi.middleware.gzip import GZipMiddleware
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+from starlette.middleware.sessions import SessionMiddleware
+app.add_middleware(SessionMiddleware, secret_key="secret-string")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", scopes={})
 
