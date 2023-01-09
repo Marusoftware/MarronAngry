b80357bbd7144cd34de8a97f1881abc0372703f0
Maruo.S <raspi-maru2004@outlook.jp>
2023-01-10 00:20:45 +0900

    signin

--- client/src/openapi/index.ts
+++ client/src/openapi/index.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 
+import { get, writable } from 'svelte/store';
 import { AuthApi, UserApi } from './apis';
 import { Configuration } from './runtime';
 
@@ -8,8 +9,15 @@ export * from './runtime';
 export * from './apis';
 export * from './models';
 
+export const accessToken=writable("")
+
+async function getAccessToken(){
+    return "Bearer "+get(accessToken)
+}
+
 const apiConfig=new Configuration({
-    basePath: "/api/v1"
+    basePath: "/api/v1",
+    accessToken: getAccessToken
 })
 
 export const authAPI = new AuthApi(apiConfig);
--- client/src/pages/Signin.svelte
+++ client/src/pages/Signin.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script lang="ts">
   import {
     Grid,
     Row,
@@ -11,6 +11,27 @@
     Link,
   } from "carbon-components-svelte";
   import Google from "svelte-material-icons/Google.svelte";
+  
+  import { form, field } from 'svelte-forms';
+  import { required } from 'svelte-forms/validators';
+  import { authAPI, userAPI, accessToken } from "../openapi";
+
+  const name = field('name', '', [required()])
+  const password = field('password', '', [required()])
+  const loginForm = form(name, password)
+
+  async function submit(e:Event) {
+    e.preventDefault()
+    await loginForm.validate()
+    if(!$loginForm.valid){
+      return
+    }
+    const token=await authAPI.authSignin({
+      username:$name.value,
+      password:$password.value
+    })
+    accessToken.set(token.accessToken)
+  }
 </script>
 
 <Grid>
@@ -19,22 +40,25 @@
       <h1>初めての方と<br />他のサービスでのログインをされる方</h1>
       <Button>Marusoftwareアカウントを作成</Button>
       <Tile>
-        <Button icon={Google} iconDescription="Signin/Signup with Google" />
+        <Button icon={Google} iconDescription="Signin/Signup with Google" href="/api/v1/auth/sso/google" />
       </Tile>
     </Column>
     <Column>
       <h1>Marusoftwareアカウントをお持ちの方</h1>
-      <Form on:submit>
+      <Form on:submit={submit}>
         <TextInput
           labelText="User name or Email"
           placeholder="Enter user name or email..."
+          bind:value={$name.value}
+          invalid={$name.invalid}
+          invalidText={$name.errors.join(", ")}
         />
-        <PasswordInput labelText="Password" placeholder="Enter password..." />
+        <PasswordInput labelText="Password" placeholder="Enter password..." bind:value={$password.value} invalid={$password.invalid} invalidText={$password.errors.join(", ")} />
         サインインすると、
         <Link inline href="https://marusoftware.net/privacypolicy.html">
           プライバシーポリシー
         </Link>に同意したこととなります。予めご確認ください。<br />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" disabled={!$loginForm.valid} >サインイン</Button>
       </Form>
     </Column>
   </Row>
