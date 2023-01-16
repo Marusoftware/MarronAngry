94fc4ab04a1444995aae8b085ba784bfc7d4ba1b
Maruo.S <raspi-maru2004@outlook.jp>
2023-01-17 03:56:19 +0900

    password visible

--- client/src/components/Field.svelte
+++ client/src/components/Field.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { Label, Input, Helper } from "flowbite-svelte"
     import type { InputType } from "flowbite-svelte/types";
+    import { Eye, EyeSlash } from "svelte-heros-v2";
+    import { onMount } from "svelte";
     export let labelText=""
     export let invalidText=""
     export let invalid=false
@@ -8,11 +10,40 @@
     export let id:string=""
     export let type:InputType="text"
     export let placeholder=""
+    let password:boolean=false
+
+    onMount(()=>{
+        if(type=="password"){
+            password=true
+        } else{
+            password=false
+        }
+    })
+
+    function changeVisible(){
+        if(type=="password"){
+            type="text"
+        } else {
+            type="password"
+        }
+    }
 </script>
 
 <div class="mb-6">
     <Label for={id} color={invalid ? "red" : "gray"} class="block mb-2" >{labelText}</Label>
-    <Input id={id} type={type} bind:value={value} color={invalid ? "red" : "base"} placeholder={placeholder} ></Input>
+    <Input id={id} type={type} bind:value={value} color={invalid ? "red" : "base"} placeholder={placeholder} {...$$props} >
+        <svelte:fragment slot="right">
+            {#if password}
+                <button on:click={changeVisible}>
+                    {#if type=="password"}
+                    <EyeSlash />
+                    {:else}
+                    <Eye />
+                    {/if}
+                </button>
+            {/if}
+        </svelte:fragment>
+    </Input>
     {#if invalid}
     <Helper color={invalid ? "red" : "gray"} >{invalidText}</Helper>
     {/if}
