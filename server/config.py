1d0818dd7985d80741a9cab524737ed97ce2d433
Maruo.S <raspi-maru2004@outlook.jp>
2023-04-14 00:20:01 +0900

    add sendgrid mail support

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     postgres_user:str
     postgres_password:str
     postgres_db:str
+    mail:str
     
     class Config:
         env_file = ".env"
--- server/mail.py
+++ server/mail.py
@@ -1,7 +1,13 @@
 import aiosendgrid
 from .config import Settings
+from sendgrid.helpers.mail import Content, Email, Mail, To
 
 config=Settings()
 
-with aiosendgrid.AsyncSendGridClient(api_key="") as sendgrid:
-    pass
+async def send_mail(from_address, to_address, subject, content, content_type="text/plain"):
+    from_email = Email(from_address,)
+    to_email = To(to_address)
+    content = Content(content_type, content)
+    mail = Mail(from_email, to_email, subject, content)
+    async with aiosendgrid.AsyncSendGridClient(api_key=config.mail) as client:
+        response = await client.send_mail_v3(body=mail.get())
