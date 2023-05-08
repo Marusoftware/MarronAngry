7ab79117ef0ad2d78cc8576aaaf433105b245896
Maruo.S <raspi-maru2004@outlook.jp>
2023-05-08 12:24:02 +0900

    Revert "fmm..."
    This reverts commit a0dd147f8be692e4f108877a7eb30d387df392b7.

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,15 +1,9 @@
 <script type="ts">
     import { Heading } from "flowbite-svelte";
     import { user } from "../utils/store";
-    import { SvelteGantt, SvelteGanttTable } from 'svelte-gantt';
-    import { onMount } from "svelte";
-    onMount(() => {
-        const gantt = new SvelteGantt({ target: document.getElementById('gantt')});
-    });
 </script>
 <Heading tag="h1">
     Welcome to Marron{#if $user}
         , {$user.fullname}!
     {/if}
-    <div id="gantt"></div>
 </Heading>
